9e96728d74be119b7d5259c5dd2de8374fa11f57
Ruben <ruben1984@hotmail.com>
2023-11-23 19:38:54 +0100

    Practica Acceso datos para conectar a la base de datos formula1 de aws, conecta pero no funciona

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,23 +6,48 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://databaseformula1.cdenajzkqjrp.us-east-1.rds.amazonaws.com:5432/formula1";
+        // Datos de conexión a la base de datos
+        String dbUrl = "jdbc:postgresql://databaseformula1.cdenajzkqjrp.us-east-1.rds.amazonaws.com:5432/formula1";
         String user = "postgres";
         String password = "1029384756";
 
+        Connection connection = null;
         try {
-            Connection connection = DriverManager.getConnection(url, user, password);
+            // Estableciendo la conexión con la base de datos
+            connection = DriverManager.getConnection(dbUrl, user, password);
+            System.out.println("Conexion realizada");
+            // Desactivando auto-commit para manejar la transacción manualmente
             connection.setAutoCommit(false);
 
-            insertarEquipo(connection);
-            // Asumiendo que obtenemos el ID del equipo...
-            int equipoId = obtenerEquipoId(connection, "Seat F1");
+            // Datos del equipo
+            String constructorRef = "seat_f1"; // Ajusta estos valores según tus necesidades
+            String name = "Seat F1";
+            String nationality = "Española";
+            String teamUrl = "http://ejemplo.com/seat_f1";
 
-            insertarPiloto(connection, "Carlos Sainz", "CSZ", equipoId);
-            insertarPiloto(connection, "Manuel Aloma", "ALM", equipoId);
+
+            // Insertar equipo
+            int equipoId = insertarEquipo(connection, constructorRef, name, nationality, teamUrl);
+            if (equipoId == -1) {
+                // Si el equipo ya existe, obtener su ID
+                equipoId = obtenerEquipoId(connection, name);
+            }
+            Date dobCarlos = Date.valueOf("1994-09-01"); // Fecha de nacimiento de Carlos Sainz
+            String urlCarlos = "http://ejemplo.com/carlos_sainz";
+
+            Date dobManuel = Date.valueOf("1990-05-20"); // Fecha de nacimiento de Manuel Aloma
+            String urlManuel = "http://ejemplo.com/manuel_aloma";
+
+            // Insertar el primer piloto
+            insertarPiloto(connection, "CSZ", "Carlos", "Sainz", dobCarlos, "Española", equipoId, urlCarlos);
+
+            // Insertar el segundo piloto
+            insertarPiloto(connection, "ALM", "Manuel", "Aloma", dobManuel, "Española", equipoId, urlManuel);
+
 
             connection.commit();
         } catch (SQLException e) {
+            // En caso de error, revertir los cambios
             try {
                 if (connection != null) {
                     connection.rollback();
@@ -31,27 +56,70 @@ public class Main {
                 ex.printStackTrace();
             }
             e.printStackTrace();
-        }
-    }
-
-    private static void insertarEquipo(Connection connection) throws SQLException {
-        String sql = "INSERT INTO equipos (nombre) VALUES ('Seat F1') " +
-                "ON CONFLICT (id) DO NOTHING RETURNING id;";
-        try (Statement stmt = connection.createStatement()) {
-            ResultSet rs = stmt.executeQuery(sql);
-            if (rs.next()) {
-                int equipoId = rs.getInt(1);
-                // Usar equipoId para operaciones futuras...
+        } finally {
+            // Cerrar la conexión
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
         }
     }
-    private static void insertarPiloto(Connection connection, String nombre, String codigo, int equipoId) throws SQLException {
-        String sql = "INSERT INTO pilotos (nombre, codigo, equipo_id) VALUES (?, ?, ?);";
+
+    private static int insertarEquipo(Connection connection, String constructorRef, String name, String nationality, String url) throws SQLException {
+        // SQL para insertar un equipo con UPSERT
+        String sql = "INSERT INTO constructors (constructorref, name, nationality, url) VALUES (?, ?, ?, ?) " +
+                "ON CONFLICT (constructorref) DO NOTHING RETURNING constructorid;";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, constructorRef);
+            stmt.setString(2, name);
+            stmt.setString(3, nationality);
+            stmt.setString(4, url);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                // Retorna el ID del constructor insertado
+                return rs.getInt(1);  // Aquí se corrige para obtener el primer valor del ResultSet
+            } else {
+                System.out.println("El constructor ya existe y no se insertó.");
+                return -1; // Retorna -1 si el constructor ya existe
+            }
+        }
+    }
+
+
+    private static int obtenerEquipoId(Connection connection, String equipoNombre) throws SQLException {
+        // Corrección en el nombre de la columna: de 'nombre' a 'name'
+        String sql = "SELECT constructorid FROM constructors WHERE name = ?;";
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, nombre);
-            pstmt.setString(2, codigo);
-            pstmt.setInt(3, equipoId);
+            pstmt.setString(1, equipoNombre);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("constructorid");
+            } else {
+                throw new SQLException("Equipo no encontrado");
+            }
+        }
+    }
+    private static void insertarPiloto(Connection connection, String code, String forename, String surname, Date dob, String nationality, int constructorId, String url) throws SQLException {
+        // SQL para insertar un piloto
+        String sql = "INSERT INTO drivers (code, forename, surname, dob, nationality, constructorid, url) VALUES (?, ?, ?, ?, ?, ?, ?);";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, code);
+            pstmt.setString(2, forename);
+            pstmt.setString(3, surname);
+            pstmt.setDate(4, dob);
+            pstmt.setString(5, nationality);
+            pstmt.setInt(6, constructorId);
+            pstmt.setString(7, url);
             pstmt.executeUpdate();
         }
     }
+
+
+
+
+
 }
